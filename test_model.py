4570ae3d23ced5e452b7751bb1058f380ec0e2fe
Vishal Manam <88299493+VishalManam@users.noreply.github.com>
2023-12-28 12:30:24 +0530

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO('/home/vishal/license_plate_detector/runs/detect/train/weights/best.ptbest.pt')
+model = YOLO('/home/vishal/license_plate_detector/runs/detect/train/weights/best.pt')
 image_paths = ['img1.jpg', 'img2.jpg', 'img3.jpg', ..., 'img30.jpg']
 
 for path in image_paths:
